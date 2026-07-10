06ec24e9814d3dce0cd851ba49b5a7a1720072b7
lucaslealgd <lucaslealsantos@poli.ufrj.br>
2026-07-09 21:26:55 -0300

    Replace test text with note description

--- .quartz/plugins/recent-notes/dist/components/index.js
+++ .quartz/plugins/recent-notes/dist/components/index.js
@@ -527,7 +527,7 @@ var RecentNotes_default = ((userOpts) => {
         return /* @__PURE__ */ u2("li", { class: "recent-li", children: /* @__PURE__ */ u2("div", { class: "section", children: [
           /* @__PURE__ */ u2("div", { class: "desc", children: /* @__PURE__ */ u2("h3", { children: /* @__PURE__ */ u2("a", { href: resolveRelative(slug2, page.slug), class: "internal", children: title }) }) }),
           page.dates && getDate(withResolvedDateType(page, cfg)) && /* @__PURE__ */ u2("p", { class: "meta", children: /* @__PURE__ */ u2("time", { datetime: getDate(withResolvedDateType(page, cfg)).toISOString(), children: formatDate(getDate(withResolvedDateType(page, cfg)), locale) }) }),
-          description && /* @__PURE__ */ u2("p", { class: "recent-summary", children: "TESTES 2" }),
+          description && /* @__PURE__ */ u2("p", { class: "recent-summary", children: description }),
           opts.showTags && /* @__PURE__ */ u2("ul", { class: "tags", children: tags.map((tag) => /* @__PURE__ */ u2("li", { children: /* @__PURE__ */ u2("a", { class: "internal tag-link", href: resolveRelative(slug2, `tags/${tag}`), children: tag }) })) })
         ] }) });
       }) }),
